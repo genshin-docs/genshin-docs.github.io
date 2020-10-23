82934b7ef2dbbe846c2222fcb4caa1c7f15eee4f
Genshin Docs <genshin.docs@gmail.com>
2020-10-23 23:56:34 +0300

    Updates

---
+++ precache-manifest.f790ea3119fa43978da2e06f1f112df3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9a7546b10c4aa1caf2fd6b5758bd32",
+    "url": "/genshin_docs/index.html"
+  },
+  {
+    "revision": "ef1d2fb2f16d749431eb",
+    "url": "/genshin_docs/static/css/3.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "a264437a1e378f47e72d",
+    "url": "/genshin_docs/static/css/main.74db2b80.chunk.css"
+  },
+  {
+    "revision": "fc1849d3b41fb0f54bc0",
+    "url": "/genshin_docs/static/js/0.a168b2ea.chunk.js"
+  },
+  {
+    "revision": "ef1d2fb2f16d749431eb",
+    "url": "/genshin_docs/static/js/3.8eed5907.chunk.js"
+  },
+  {
+    "revision": "dbf09cfb24c6d4e52d5ef701d77d8ec9",
+    "url": "/genshin_docs/static/js/3.8eed5907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714fb9b0c74c2268ba40",
+    "url": "/genshin_docs/static/js/4.af23b8a5.chunk.js"
+  },
+  {
+    "revision": "7bc0a96630a4225cae8a",
+    "url": "/genshin_docs/static/js/5.e54653c3.chunk.js"
+  },
+  {
+    "revision": "ca7dd2eb48b8004802c5",
+    "url": "/genshin_docs/static/js/6.8af84ae7.chunk.js"
+  },
+  {
+    "revision": "a264437a1e378f47e72d",
+    "url": "/genshin_docs/static/js/main.fc0bf333.chunk.js"
+  },
+  {
+    "revision": "6327dfe42209d7673d4e",
+    "url": "/genshin_docs/static/js/runtime-main.4a8401ca.js"
+  },
+  {
+    "revision": "0f72b1ce5ac835e488a225d9d43b6da7",
+    "url": "/genshin_docs/static/media/doc.0f72b1ce.md"
+  },
+  {
+    "revision": "c6b662c37721f0b5c49bb9dee1a7d63d",
+    "url": "/genshin_docs/static/media/energy.c6b662c3.md"
+  },
+  {
+    "revision": "801ad590a9a4c729d6662452e64f92f9",
+    "url": "/genshin_docs/static/media/resistance.801ad590.md"
+  }
+]);
